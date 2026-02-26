a57c35a554a11f502890a81784632ef8b83bff12
Rithvik Kumar <rithvikkumar30@gmail.com>
2026-02-26 11:50:06 +0530

    user service completed

--- services/user/src/routes/user.ts
+++ services/user/src/routes/user.ts
@@ -1,7 +1,14 @@
 import express from "express";
-import { loginUser } from "../controllers/user.js";
+import { getUserProfile, loginUser, myProfile, updateProfilePic, updateUser } from "../controllers/user.js";
+import { isAuth } from "../middleware/isAuth.js";
+import uploadFile from "../middleware/multer.js";
 
 const router = express.Router();
 router.post("/login", loginUser)
+router.get("/me", isAuth, myProfile)
+router.post("/user/update", isAuth, updateUser)
+router.get("/user/:id", getUserProfile)
+router.post("/user/update/pic", isAuth, uploadFile, updateProfilePic)
+
 
 export default router;
--- services/user/src/server.ts
+++ services/user/src/server.ts
@@ -2,9 +2,21 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDb from "./utils/db.js";
 import userRoutes from "./routes/user.js"
+import {v2 as cloudinary} from "cloudinary";
 
+dotenv.config();
 
-dotenv.config()
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME as string,
+  api_key: process.env.CLOUD_API_KEY as string,
+  api_secret: process.env.CLOUD_API_SECRET as string,
+});
+
+console.log("☁️  Cloudinary config test:", {
+  cloud_name: cloudinary.config().cloud_name,
+  api_key_exists: !!cloudinary.config().api_key,
+  api_secret_exists: !!cloudinary.config().api_secret,
+});
 
 const port = process.env.PORT;
 const app = express();
