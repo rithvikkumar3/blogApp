c5eab33560f24838c3dbbbd8538e19d58832a1d7
Rithvik Kumar <rithvikkumar30@gmail.com>
2026-02-26 13:37:59 +0530

    initial setup for author microservice

---
+++ services/author/src/server.ts
@@ -0,0 +1,12 @@
+import express from "express"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const app = express()
+
+const port = process.env.PORT;
+
+app.listen(port, ()=>{
+    console.log(` ✅ Server is live on http://localhost:${port}`);
+})
