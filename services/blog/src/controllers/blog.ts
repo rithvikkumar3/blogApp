4905233ea7c72990acb50356f1a7eeff4cc89b64
Rithvik Kumar <rithvikkumar30@gmail.com>
2026-02-27 00:16:39 +0530

    added redis to the blog microservice

--- services/blog/src/controllers/blog.ts
+++ services/blog/src/controllers/blog.ts
@@ -1,32 +1,73 @@
 import axios from "axios";
 import { sql } from "../utils/db.js";
 import tryCatch from "../utils/tryCatch.js";
+import { redisClient } from "../server.js";
 
-export const getAllBlogs = tryCatch(async(req,res)=>{
-    const {searchQuery, category} = req.query
-    let blogs 
-    if(searchQuery && category) {
+export const getAllBlogs = tryCatch(async (req, res) => {
+    const { searchQuery = "", category = "" } = req.query
+
+    const cacheKey = `blogs: ${searchQuery}:${category}`;
+
+    const cached = await redisClient.get(cacheKey);
+
+    if (cached) {
+        console.log("Serving from redis cache");
+        res.json(JSON.parse(cached))
+        return;
+    }
+
+    let blogs
+    if (searchQuery && category) {
         blogs = await sql`SELECT * FROM blogs WHERE(title ILIKE ${"%" + searchQuery + "%"} 
         OR description ILIKE ${"%" + searchQuery + "%"}) 
         AND category = ${category}
         ORDER BY created_at DESC`
     }
-    else if(searchQuery){
+    else if (searchQuery) {
         blogs = await sql`SELECT * FROM blogs WHERE(title ILIKE ${"%" + searchQuery + "%"} 
         OR description ILIKE ${"%" + searchQuery + "%"}) 
         ORDER BY created_at DESC`
     }
-    else{
+    else if (category) {
+        blogs = await sql`SELECT * FROM blogs WHERE category = ${category} 
+        ORDER BY created_at DESC`
+    }
+    else {
         blogs = await sql`SELECT * FROM blogs ORDER BY created_at DESC`
     }
+    console.log("Serving from DB");
+    await redisClient.set(cacheKey, JSON.stringify(blogs), { EX: 3600 })
+
     res.json(blogs);
 })
 
 
-export const getSingleBlog = tryCatch(async(req,res)=>{
-    const {id} = req.params
-    const blog = await sql`SELECT * FROM blogs WHERE id = ${id}`
-    const {data} = await axios.get(`${process.env.USER_SERVICE}/api/v1/user/${blog[0].author}`)
+export const getSingleBlog = tryCatch(async (req, res) => {
 
-    res.json({blog: blog[0], author: data})
+    const blogid = req.params.id
+
+    const cacheKey = `blog:${blogid}`
+
+    const cached = await redisClient.get(cacheKey)
+
+    if (cached) {
+        console.log("Serving single blog from redis cache");
+        res.json(JSON.parse(cached))
+        return;
+    }
+
+    const blog = await sql`SELECT * FROM blogs WHERE id = ${blogid}`
+    if(blog.length === 0){
+        res.status(404).json({
+            message: "No blog with this ID",
+        });
+        return;
+    }
+
+    const { data } = await axios.get(`${process.env.USER_SERVICE}/api/v1/user/${blog[0]!.author}`)
+
+    const responseData = { blog: blog[0], author: data }
+    await redisClient.set(cacheKey, JSON.stringify(responseData), {EX: 3600})
+
+    res.json(responseData)
 })
--- services/blog/src/server.ts
+++ services/blog/src/server.ts
@@ -1,13 +1,25 @@
 import express from "express"
 import dotenv from "dotenv"
-
 import blogRoutes from "./routes/blog.js"
+import {createClient} from "redis"
+
 
 dotenv.config()
 
 const app = express()
 
 const port = process.env.PORT
+
+export const redisClient = createClient({
+    url: process.env.REDIS_URL as string,
+})
+
+redisClient
+    .connect()
+    .then(()=>console.log("✅ Connected to redis!"))
+    .catch(console.error)
+
+    
 
 app.use("/api/v1", blogRoutes)
 
