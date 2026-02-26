915b58b6f65387dd3aaf1c9d9f041dc5edcb80c9
Rithvik Kumar <rithvikkumar30@gmail.com>
2026-02-26 15:52:37 +0530

    author service completed

--- services/author/src/server.ts
+++ services/author/src/server.ts
@@ -1,12 +1,71 @@
 import express from "express"
 import dotenv from "dotenv"
+import { sql } from "./utils/db.js";
+import blogRoutes from "./routes/blog.js"
+import {v2 as cloudinary} from "cloudinary";
+
 
 dotenv.config()
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME as string,
+  api_key: process.env.CLOUD_API_KEY as string,
+  api_secret: process.env.CLOUD_API_SECRET as string,
+});
+
+console.log("☁️  Cloudinary config test:", {
+  cloud_name: cloudinary.config().cloud_name,
+  api_key_exists: !!cloudinary.config().api_key,
+  api_secret_exists: !!cloudinary.config().api_secret,
+});
 
 const app = express()
 
 const port = process.env.PORT;
 
-app.listen(port, ()=>{
-    console.log(` ✅ Server is live on http://localhost:${port}`);
+async function initDB() {
+    try {
+        await sql`
+        CREATE TABLE IF NOT EXISTS blogs(
+        id SERIAL PRIMARY KEY,
+        title VARCHAR(255) NOT NULL,
+        description VARCHAR(255) NOT NULL,
+        blogContent TEXT NOT NULL,
+        image VARCHAR(255) NOT NULL,
+        category VARCHAR(255) NOT NULL,
+        author VARCHAR(255) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );`;
+        await sql`
+        CREATE TABLE IF NOT EXISTS comments(
+        id SERIAL PRIMARY KEY,
+        comment VARCHAR(255) NOT NULL,
+        userid VARCHAR(255) NOT NULL,
+        username VARCHAR(255) NOT NULL,
+        blogid VARCHAR(255) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );`;
+        await sql`
+        CREATE TABLE IF NOT EXISTS savedBlogs(
+        id SERIAL PRIMARY KEY,
+        userid VARCHAR(255) NOT NULL,
+        blogid VARCHAR(255) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );`;
+        console.log("✅ Database initialized successfully");
+        
+
+    } catch (error) {
+        console.log("Error initDB", error);
+        
+    }
+}
+
+app.use("/api/v1", blogRoutes)
+
+initDB().then(()=>{
+    app.listen(port, () => {
+    console.log(`👍 Server is live on http://localhost:${port}`);
 })
+})
+
